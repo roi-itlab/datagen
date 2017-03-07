861aae176d0de80e627fb444e958f4b365d34bc1
vladikM <mamaev.vlad1@gmail.com>
2017-03-07 17:59:42 +0300

    edges, trip.calcEdges

--- src/main/java/org/roi/itlab/cassandra/Trip.java
+++ src/main/java/org/roi/itlab/cassandra/Trip.java
@@ -3,10 +3,12 @@ package org.roi.itlab.cassandra;
 import com.graphhopper.GHRequest;
 import com.graphhopper.GHResponse;
 import com.graphhopper.GraphHopper;
+import com.graphhopper.json.geo.Point;
 import com.graphhopper.routing.Path;
+import com.graphhopper.storage.NodeAccess;
+import com.graphhopper.util.EdgeIteratorState;
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Trip {
     private final Poi from;
@@ -44,5 +46,22 @@ public class Trip {
             throw new IllegalStateException("routing failed");
         }
         return paths.get(0);
+    }
+
+    public Map<Edge, Long> calcEdges(GraphHopper hopper) {
+        Map<Edge, Long> map = new HashMap<>();
+        Path path = calcPath(hopper);
+        NodeAccess nodes = hopper.getGraphHopperStorage().getBaseGraph().getNodeAccess();
+        for (EdgeIteratorState edge :
+                path.calcEdges()) {
+            double x1 = nodes.getLongitude(edge.getAdjNode());
+            double x2 = nodes.getLongitude(edge.getBaseNode());
+            double x3 = nodes.getLat(edge.getAdjNode());
+            double x4 = nodes.getLat(edge.getBaseNode());
+            int id = edge.getEdge();
+            long time = startTime;
+            map.put(new Edge(id,new Point(x4,x2),new Point(x3,x1)), time);
+        }
+        return map;
     }
 }
