92fb3818c8870afee7b2795eb0ff07f6a18f4a64
Mikhail Kuperman <mkuperman@users.noreply.github.com>
2017-03-18 17:50:41 +0300

    Update GenerateLocations.java

--- src/main/java/org/roi/itlab/cassandra/GenerateLocations.java
+++ src/main/java/org/roi/itlab/cassandra/GenerateLocations.java
@@ -144,4 +144,20 @@ public class GenerateLocations {
 
     }
 
-}
+   //returns list of random points generated nearby pois from the source list
+    public static List<Point> generateRandomLocations(int numberOfLocations, List<Poi> sourceList, int radius) {
+        Random r = new Random(System.currentTimeMillis());
+        List<Point> resultList = new ArrayList<>();
+        for (int i = 0; i < numberOfLocations; i++){
+            int center = r.nextInt(sourceList.size());
+            double distance = r.nextDouble()*radius;
+            double angle = r.nextDouble()*2*Math.PI;
+            double dlat = distance*Math.cos(angle)/metersPerLatDeg;
+            double dlng = distance*Math.sin(angle)/metersPerLngDeg;
+            double lat = sourceList.get(center).getLoc().getLatitude() + dlat;
+            double lng = sourceList.get(center).getLoc().getLongitude() + dlng;
+            resultList.add(GeoJson.point(lat, lng));
+        }
+        return resultList;
+     }
+ }
