78fd9d4e4a113b271877eeaa1451a63fe9106642
Mikhail Kuperman <mikhail.kuperman@returnonintelligence.com>
2017-03-28 02:26:19 +0300

    Fix test

--- src/test/java/org/roi/itlab/cassandra/PersonGeneratorIT.java
+++ src/test/java/org/roi/itlab/cassandra/PersonGeneratorIT.java
@@ -14,7 +14,7 @@ import java.util.List;
 /**
  * Created by Vadim on 26.02.2017.
  */
-public class TestRersonBuilder {
+public class PersonGeneratorIT {
     List<Person> list = new ArrayList<>();
     int mean_mean = 0;
 
